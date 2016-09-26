833afa4298549b3a04044a55e44e789baab5d87c
Michał Jażdżewski <mjazdzewski@soldevelo.com>
2016-09-26 15:24:25 +0200

    Test Chrome install #5

--- config_services.sh
+++ config_services.sh
@@ -19,3 +19,5 @@ sudo apt-get install -y xvfb
 sudo apt-get install libxss1 libappindicator1 libindicator7
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
+sudo apt-get install -f
+sudo dpkg -i google-chrome*.deb
