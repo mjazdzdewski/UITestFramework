709bc1330bb4cff5d9cdea5772ea6b60e26c9440
Michał Jażdżewski <mjazdzewski@soldevelo.com>
2016-09-27 10:11:56 +0200

    Test Chrome install #14

--- config_services.sh
+++ config_services.sh
@@ -17,7 +17,7 @@ sudo chown -R tomcat7:tomcat7 /var/lib/tomcat7/ /usr/share/tomcat7/ /var/log/tom
 #Installation and configuration of Xvfb
 sudo apt-get install -y xvfb -qq
 wget http://security.ubuntu.com/ubuntu/pool/universe/c/chromium-browser/chromium-browser_52.0.2743.116-0ubuntu0.16.04.1.1250_amd64.deb
-sudo dpkg -i chromium-browser*.deb
+#sudo dpkg -i chromium-browser*.deb
 #sudo apt-get install libxss1 libappindicator1 libindicator7 libstdc++6 fonts-liberation lsb-base
 #wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 #sudo dpkg -i google-chrome*.deb
