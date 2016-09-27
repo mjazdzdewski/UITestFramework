6e2921d39d7cb0326d64815a62e975ffd8438b4b
Michał Jażdżewski <mjazdzewski@soldevelo.com>
2016-09-27 10:08:22 +0200

    Test Chrome install #13

--- config_services.sh
+++ config_services.sh
@@ -21,5 +21,6 @@ sudo dpkg -i chromium-browser*.deb
 #sudo apt-get install libxss1 libappindicator1 libindicator7 libstdc++6 fonts-liberation lsb-base
 #wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 #sudo dpkg -i google-chrome*.deb
-#sudo apt-get install -f
+sudo apt-get install -f
+sudo dpkg -i chromium-browser*.deb
 #sudo dpkg -i google-chrome*.deb
